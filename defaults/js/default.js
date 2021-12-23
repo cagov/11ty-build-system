538bf19e546d109c776969308b1784586a9e4a1a
Jon Jensen <github@id.xjensen.com>
2021-12-22 17:12:16 -0800

    Remove templates and layouts

--- defaults/js/default.js
+++
@@ -1,9 +0,0 @@
-import '@cagov/ds-accordion';
-import '@cagov/ds-feedback';
-import '@cagov/ds-minus';
-import '@cagov/ds-pagination';
-import '@cagov/ds-plus';
-import '@cagov/ds-dropdown-menu';
-import '@cagov/ds-content-navigation';
-import '@cagov/ds-link-icon/src/index.js';
-import '@cagov/ds-back-to-top/src/index.js';
